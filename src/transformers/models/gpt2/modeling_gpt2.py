7fa5566722393c4ae4e7f8b3310ebbf47ccdf382
omri.s <omri.s@hiredscore.com>
2023-07-06 18:38:28 +0300

    Update modeling_gpt2.py

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -1250,10 +1250,11 @@ class GPT2LMHeadModel(GPT2PreTrainedModel):
             shift_labels = labels[..., 1:].contiguous()
 
             multi_label_loss = self.get_multi_label_loss(shift_labels, shift_multi_label_logits)
-            multi_label_hidden_states = self.get_multi_label_pred(torch.sigmoid(shift_multi_label_logits))[0]
-            cross_attention_hidden_states = self.cross_attention(hidden_states, multi_label_hidden_states, multi_label_hidden_states)
-            cat_hidden_states= torch.cat([cross_attention_hidden_states, hidden_states], dim=2)
-            lm_logits = self.lm_multi_label_head(cat_hidden_states)
+            # multi_label_hidden_states = self.get_multi_label_pred(torch.sigmoid(shift_multi_label_logits))[0]
+            # cross_attention_hidden_states = self.cross_attention(hidden_states, multi_label_hidden_states, multi_label_hidden_states)
+            # cat_hidden_states= torch.cat([cross_attention_hidden_states, hidden_states], dim=2)
+            # lm_logits = self.lm_multi_label_head(cat_hidden_states)
+            lm_logits = self.lm_head(hidden_states)
             shift_logits = lm_logits[..., :-1, :].contiguous()
 
             # Flatten the tokens
