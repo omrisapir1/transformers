389060c6e2c63c343628e280ef334e1d3bb111bd
omri.s <omri.s@hiredscore.com>
2023-07-11 12:06:01 +0300

    Update modeling_gpt2.py

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -1255,7 +1255,7 @@ class GPT2LMHeadModel(GPT2PreTrainedModel):
             # cross_attention_hidden_states = self.cross_attention(hidden_states, multi_label_hidden_states, multi_label_hidden_states)
             # cat_hidden_states= torch.cat([cross_attention_hidden_states, hidden_states], dim=2)
             # lm_logits = self.lm_multi_label_head(cat_hidden_states)
-            lm_logits = self.fake_lm_head(hidden_states)
+            lm_logits = self.lm_head(hidden_states)
             shift_logits = lm_logits[..., :-1, :].contiguous()
 
             # Flatten the tokens
